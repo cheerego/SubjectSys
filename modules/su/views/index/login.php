52f7a7c90639663da1e8558acf5443acbcb1bf7a
placeless <41974873@qq.com>
2016-02-12 16:37:32 +0800

    su  登录

--- modules/su/views/index/login.php
+++ modules/su/views/index/login.php
@@ -5,4 +5,26 @@
  * Date: 16/2/11
  * Time: 下午11:46
  */
-echo 'login';
+?>
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Su */
+/* @var $form ActiveForm */
+?>
+<div class="site-sulogin" style="margin-top: 5%">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'username') ?>
+    <?= $form->field($model, 'password') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+    </div>
+    <?php ActiveForm::end(); ?>
+
+</div><!-- site-sulogin -->
